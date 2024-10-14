27fa80c30f0ebf0645ca4da2d458d93b305e877d
Rizalyne Asaldo <sherelyncuanan@student.laverdad.edu.ph>
2024-10-15 02:41:01 +0800

    I'm done completing the section on responding to events

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,17 @@
 import React from 'react';
 
-const products = [
-  { title: 'Cabbage', isFruit: false, id: 1 },
-  { title: 'Garlic', isFruit: false, id: 2 },
-  { title: 'Apple', isFruit: true, id: 3 },
-];
+function App() {
+  function handleClick() {
+    alert('You clicked me!');
+  }
 
-function ShoppingList() {
-  const listItems = products.map(product => (
-    <li
-      key={product.id}
-      style={{
-        color: product.isFruit ? 'magenta' : 'darkgreen'
-      }}
-    >
-      {product.title}
-    </li>
-  ));
-
-  return (
-    <ul>{listItems}</ul>
-  );
-}
-
-export default function App() {
   return (
     <div>
-      <h1>My Shopping List</h1>
-      <ShoppingList />
+      <button onClick={handleClick}>
+        Click me
+      </button>
     </div>
   );
 }
+
+export default App;
