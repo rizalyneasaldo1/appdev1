67d44fcb2eef129c301fa545104ffef3ac8bb979
Rizalyne Asaldo <sherelyncuanan@student.laverdad.edu.ph>
2024-10-15 01:48:05 +0800

    I'm done completing the section on displaying the data

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import './App.css'
+import Profile from './Profile'
+
 function App() {
   return (
     <>
       <h1>Welcome to my apps</h1>
-      <img className="avatar" src="https://i.imgur.com/yXOvdOSs.jpg" />
+      <img className="avatar" />
+      <Profile />
     </>
   )
 }
